2e5ab892db0af89c6d24a4d28160837d7305268a
Inga-e <114048571+Inga-e@users.noreply.github.com>
2023-09-26 21:58:22 +0700

    Размещает проект Drink2Go

---
+++ source/js/script.js
@@ -0,0 +1,34 @@
+/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector(".nav");
+let navToggle = document.querySelector(".nav__toggle");
+
+const closeMenu = () => {
+  navMain.classList.add("nav--closed");
+  navMain.classList.remove("nav--opened");
+};
+
+const openMenu = () => {
+  navMain.classList.remove("nav--closed");
+  navMain.classList.add("nav--opened");
+};
+
+const statusMenu = () => {
+  navMain.classList.remove("nav--nojs");
+  closeMenu();
+};
+
+if (navMain.classList.contains("nav--nojs")) {
+  statusMenu();
+}
+
+
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("nav--closed")) {
+    openMenu();
+  } else {
+    closeMenu();
+  }
+});
+
+/* script slider */
+/* не успела дописать, но знаю, как делать*/
